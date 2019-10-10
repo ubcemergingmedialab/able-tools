393e8f940ce6e65da271cb84681a963cd0c75277
EMLELEVEN <uemlelev@ead.ubc.ca>
2019-10-10 13:52:58 -0700

    Merge Scripts

---
+++ Assets/Tools/1. LightSwitches - Shawn/script/TriggerTestOFF.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTestOFF : MonoBehaviour
+{
+    public Light light;
+
+    void OnTriggerEnter(Collider other)
+    {
+        light.enabled = false;
+    }
+
+}
---
+++ Assets/Tools/1. LightSwitches - Shawn/script/TriggerTestON.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerTestON : MonoBehaviour
+{
+    public Light light;
+
+    void OnTriggerEnter(Collider other)
+    {
+        light.enabled = true;
+    }
+
+}
