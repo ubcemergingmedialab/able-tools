6116b4a53ed127e7aef823e88fc1514f83e31c5f
kylemas <kyle.mas567@gmail.com>
2019-10-16 15:46:13 -0700

    Created the scripts and objects for LightSwitch
    Created a lightswitch script that turns on the light when the player rolls over the white square trigger.

---
+++ Assets/SampleAssets/Scripts/Trigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    public Light light;
+    private bool on = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        light.enabled = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        light.enabled = false;
+    }
+}
