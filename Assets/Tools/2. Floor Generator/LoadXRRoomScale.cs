6c4989887bff7c4628d9feb0dbd571d3527eb847
unoctium1 <unoctium1@gmail.com>
2019-10-18 15:26:41 -0700

    Add steam vr and stub classes for tool 2

---
+++ Assets/Tools/2. Floor Generator/LoadXRRoomScale.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+namespace EML
+{
+    namespace FloorGen
+    {
+
+        /// <summary>
+        /// Provides a cross platform way of accessing the player's room scale set up
+        /// </summary>
+        public class LoadXRRoomScale
+        {
+            
+            public Vector3[] GetFloor()
+            {
+                if(XRDevice.GetTrackingSpaceType() == TrackingSpaceType.RoomScale)
+                {
+
+                }
+                else { return null; }
+                return null;
+            }
+
+        }
+    }
+}
