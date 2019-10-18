b911bb0decc3d375dd584a44d83483ebe2cf3588
AbelW <unoctium1@gmail.com>
2019-10-18 16:33:50 -0700

    add oculus

--- Assets/Tools/2. Floor Generator/LoadXRRoomScale.cs
+++ Assets/Tools/2. Floor Generator/LoadXRRoomScale.cs
@@ -14,14 +14,29 @@ namespace EML
         public class LoadXRRoomScale
         {
             
-            public Vector3[] GetFloor()
+            public static Vector3[] GetFloor()
             {
                 if(XRDevice.GetTrackingSpaceType() == TrackingSpaceType.RoomScale)
                 {
-
+                    if(XRSettings.loadedDeviceName == "Oculus")
+                    {
+                       // return OculusGetFloor();
+                    }
                 }
                 else { return null; }
                 return null;
+            }
+
+            public static string GetVRSDK()
+            {
+                return XRSettings.loadedDeviceName;
+            }
+
+            public static OVRPlugin.Vector3f[] OculusGetFloor()
+            {
+                OVRPlugin.BoundaryGeometry geo = OVRPlugin.GetBoundaryGeometry(OVRPlugin.BoundaryType.PlayArea);
+                return geo.Points;
+                //return OVRBoundary.GetGeometry(OVRBoundary.BoundaryType.PlayArea);
             }
 
         }
---
+++ Assets/Tools/2. Floor Generator/TestScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TestScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(EML.FloorGen.LoadXRRoomScale.GetVRSDK());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
