149a477c29f5c8f3eee502136dc46ea9b922e96f
Boxiu Shawn Xiao <xiaobx.stu@ead.ubc.ca>
2019-11-15 15:14:21 -0800

    FloorMeshCreator Fix
    Branch Creation and Bug fix

---
+++ Assets/Tools/2. Floor Generator/Example.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    Mesh mesh;
+    Vector3[] vertices;
+    void Start()
+    {
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+    }
+
+    void Update()
+    {
+        for (var i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] += Vector3.left * Time.deltaTime;
+        }
+
+        // assign the local vertices array into the vertices array of the Mesh.
+        mesh.vertices = vertices;
+        mesh.RecalculateBounds();
+    }
+}
--- Assets/Tools/2. Floor Generator/FloorMeshCreator.cs
+++ Assets/Tools/2. Floor Generator/FloorMeshCreator.cs
@@ -6,6 +6,8 @@ public class FloorMeshCreator : MonoBehaviour
 {
 
     MeshFilter meshF;
+    public Vector3[] verts;
+    //public Vector3 lowerLeft, lowerRight, upperLeft, upperRight;
 
     // Start is called before the first frame update
     void Start()
@@ -13,26 +15,35 @@ public class FloorMeshCreator : MonoBehaviour
         meshF = GetComponent<MeshFilter>();
         Mesh mesh = meshF.mesh;
 
-        Vector3[] verts = mesh.vertices;
+        verts = mesh.vertices;
 
+       
+    
         Vector3[] newArray = EML.FloorGen.LoadXRRoomScale.GetFloor().ToArray();
 
-        for(int i = 0; i < 4; i++)
-        {
-            verts[i].x = newArray[i].x;
-            verts[i].y = newArray[i].z;
 
-        }
+
+        verts[1] = new Vector3(newArray[0].x, newArray[0].z);
+        verts[3] = new Vector3(newArray[1].x, newArray[1].z);
+        verts[0] = new Vector3(newArray[2].x, newArray[2].z);
+        verts[2] = new Vector3(newArray[3].x, newArray[3].z);
+        
+     /*   //Assign corner vectors back to vewrts array
+        verts[0] = lowerRight; // --
+        verts[1] = upperLeft;  // ++ 
+        verts[2] = lowerLeft;  // +-
+        verts[3] = upperRight; // -+
+       */
 
         mesh.vertices = verts;
-
-        
-
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+        mesh.RecalculateTangents();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
